9e7251ad7a88b166116b694161f63ba61d45548a
Ross Grambo <rossgrambo@asana.com>
2019-05-17 15:21:53 -0700

    Misc. updates to the process

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,15 @@ const PAT = process.argv[2];
 const target = process.argv[3];
 const resource = process.argv[4];
 
+let savedSecret = null;
+
 router.get('/', function(req, res, next) {
   res.status(200);
-  res.send("Send a GET request to /create-webhook to create a webhook.");
+  let url = req.protocol + '://' + req.get('host') + "/create-webhook";
+      res.send("Go <a href='" + url + "'>" + url + "</a> to create a webhook.");
 });
 
+/* CREATE WEBHOOK */
 router.get('/create-webhook', function(req, res, next) {
   console.log(":: Create Webhook - Sending POST ::");
   request.post('https://app.asana.com/api/1.0/webhooks', {
@@ -29,25 +33,71 @@ router.get('/create-webhook', function(req, res, next) {
       return
     }
 
-    console.log(":: Create Webhook - Handling Response ::");
-    console.log(subRes.statusCode);
-    console.log(subRes.headers);
+    console.log(":: Create Webhook - Got Response Code " + subRes.statusCode + " ::");
+
+    //console.log(subRes.headers);
     console.log(body);
 
+    let createdWebhook = '';
+    if (body && body.data && body.data.gid) {
+      createdWebhook = body.data.gid;
+    }
+
     res.status(200);
-    res.send("Tried to create a webhook.");
-    console.log(":: Create Webhook - Got Response Code" + subRes.statusCode + " ::");
+    if (subRes.statusCode === 201) {
+      let url = req.protocol + '://' + req.get('host') + "/delete-webhook?webhook=" + createdWebhook;
+      res.send("Created a webhook. Go to <a href='" + url + "'>" + url + "</a> to delete it.");
+    } else {
+      res.send("Failed to create a webhook.");
+    }
+
+    console.log(":: Create Webhook - Finished Handling Response ::");
   });
 });
 
-/* POST home page. */
+/* DELETE WEBHOOK */
+router.get('/delete-webhook', function(req, res, next) {
+  let webhookId = req.query.webhook;
+
+  if (!webhookId) {
+    res.send("Please supply a ?webhook");
+    return;
+  }
+
+  console.log(":: Delete Webhook - Sending DELETE ::");
+  request.delete('https://app.asana.com/api/1.0/webhooks/' + webhookId, {
+    auth: {
+      bearer: PAT
+    }
+  }, function (error, subRes, body) {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    console.log(":: Delete Webhook - Got Response Code " + subRes.statusCode + " ::");
+
+    res.status(200);
+    if (subRes.statusCode === 200) {
+      let url = req.protocol + '://' + req.get('host') + "/create-webhook";
+      res.send("Deleted a webhook. To create a new webhook, go to <a href='" + url + "'>" + url + "</a>.");
+    } else {
+      res.send("Failed to delete the webhook.");
+    }
+  });
+});
+
+/* HANDSHAKE */
 router.post('/', function(req, res, body) {
   console.log(":: Received Handshake - Handling POST ::");
-  console.log(req.headers);
-  console.log(body);
+
+  //console.log(req.headers);
+  //console.log(body);
+
   const secret = req.header('X-Hook-Secret');
   if (secret) {
     res.set('X-Hook-Secret', secret);
+    savedSecret = secret;
   }
   res.status(200);
   res.send();
